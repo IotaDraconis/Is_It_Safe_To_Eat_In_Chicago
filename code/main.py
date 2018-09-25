71bf84a47e621e5bdc2ac799cc83755602e434b9
Paul Coen <pcoen142@gmail.com>
2018-09-25 17:31:21 -0500

    Changed where datasets are described

--- code/main.py
+++ code/main.py
@@ -1,9 +1,7 @@
 '''
 Created by Paul Coen
 This script is to be used to run the clutering algorithms for CS-533 on the Chicago datasets we are using
-If not already done, please download the following datasets:
-    [url goes here] save as crime.csv
-    [url goes here] save as food_inspections.csv
+If not already done, please download the following datasets described in datasets/DatasetList.txt
 Then run prepare.py. This will clean the data for use with this script
 '''
 
