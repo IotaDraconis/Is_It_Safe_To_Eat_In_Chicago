1407e10b06e014acb37dbdb26e3f646fa0a399d5
Paul Coen <paul.coen@siu.edu>
2018-09-25 16:36:30 -0500

    This is where algorithm implementation will be

---
+++ code/main.py
@@ -0,0 +1,11 @@
+'''
+Created by Paul Coen
+This script is to be used to run the clutering algorithms for CS-533 on the Chicago datasets we are using
+If not already done, please download the following datasets:
+    [url goes here] save as crime.csv
+    [url goes here] save as food_inspections.csv
+Then run prepare.py. This will clean the data for use with this script
+'''
+
+import csv
+import scipy.cluster as cluster
