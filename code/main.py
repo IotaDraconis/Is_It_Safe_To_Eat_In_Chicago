fe879446bcc3450e991aec06ed49d6572414d1e1
Paul Coen <paul.coen@siu.edu>
2018-09-26 10:56:28 -0500

    Added loading crime into main.
    Started on plotting the data (or subsets of it)

--- code/main.py
+++ code/main.py
@@ -5,5 +5,17 @@ If not already done, please download the following datasets described in dataset
 Then run prepare.py. This will clean the data for use with this script
 '''
 
-import csv
+#import csv
+import os
 import scipy.cluster as cluster
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Read entire csv as a numpy array
+ordered_crime_path = os.path.join(os.path.dirname(__file__), '../datasets/ordered_crime.csv')
+#ordered_food_inspections_path = os.path.join(os.path.dirname(__file__), '../datasets/ordered_food_inspections.csv')
+
+crimeData = np.genfromtxt(ordered_crime_path, delimiter=',', dtype=None, encoding='utf8')
+#plt.plot(crimeData[1:100, 5], crimeData[1:100, 6], 'ro')
+#plt.axis([0, 180, 0, 180])
+#plt.show()
