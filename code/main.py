40e4011368ce9f279cc503b062579021f97a1ebb
Paul Coen <pcoen142@gmail.com>
2018-10-15 22:20:37 -0500

    Added code to plot clusters for DBSCAN. Adjust eps to see various clusters.

--- code/main.py
+++ code/main.py
@@ -38,13 +38,11 @@ crime_sample3 = crime04_df.sample(n=75000, replace=True)
 crime_sample4 = crime08_df.sample(n=75000, replace=True)
 
 # create arrays with just the data we ned for DBSCAN
-crime_sample1LatLong = crime_sample1[['latitude', 'longitude']]
-
-print(crime_sample1LatLong)
+X = crime_sample1[['latitude', 'longitude']]
 
 # Run DBSCAN on the samples
 # Need to tweak eps
-db = DBSCAN(eps=0.1, min_samples=5).fit(X=crime_sample1LatLong)
+db = DBSCAN(eps=0.01, min_samples=50).fit(X)
 core_samples_mask = np.zeros_like(db.labels_, dtype=bool)
 core_samples_mask[db.core_sample_indices_] = True
 labels = db.labels_
@@ -53,6 +51,40 @@ labels = db.labels_
 n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
 
 print('Estimated number of clusters: %d' % n_clusters_)
+
+
+# Black removed and is used for noise instead.
+unique_labels = set(labels)
+colors = [plt.cm.Spectral(each)
+          for each in np.linspace(0, 1, len(unique_labels))]
+for k, col in zip(unique_labels, colors):
+    if k == -1:
+        # Black used for noise.
+        col = [0, 0, 0, 1]
+
+    class_member_mask = (labels == k)
+
+    xy = X[class_member_mask & core_samples_mask]
+    print(xy.latitude)
+    plt.plot(xy.latitude,
+             xy.longitude,
+             '.',
+             markerfacecolor=tuple(col),
+             markeredgecolor='k',
+             markersize=14)
+
+    xy = X[class_member_mask & ~core_samples_mask]
+    plt.plot(xy.latitude,
+             xy.longitude,
+             '.',
+             markerfacecolor=tuple(col),
+             markeredgecolor='k',
+             markersize=6)
+
+plt.title('Estimated number of clusters: %d' % n_clusters_)
+plt.show()
+
+
 
 '''
 # Plot the samples
